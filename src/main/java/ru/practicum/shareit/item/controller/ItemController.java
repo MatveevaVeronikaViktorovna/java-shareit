18aa3ffc46c90a30e973fd9b270ad69e7d73ff12
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-05-26 11:31:23 +0300

    fix:исправляю замечания

--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDtoForCreate;
 import ru.practicum.shareit.item.dto.ItemDtoForUpdate;
@@ -17,22 +18,26 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ItemDtoForCreate create(@RequestHeader("X-Sharer-User-Id") Long userId,
                                    @Valid @RequestBody ItemDtoForCreate itemDtoForCreate) {
         return itemService.create(userId, itemDtoForCreate);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<ItemDtoForCreate> getAllByOwner(@RequestHeader("X-Sharer-User-Id") Long userId) {
         return itemService.getAllByOwner(userId);
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ItemDtoForCreate getById(@PathVariable Long id) {
         return itemService.getById(id);
     }
 
     @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public ItemDtoForCreate update(@RequestHeader("X-Sharer-User-Id") Long userId,
                                    @PathVariable Long id,
                                    @Valid @RequestBody ItemDtoForUpdate itemDtoForUpdate) {
@@ -40,11 +45,13 @@ public class ItemController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         itemService.delete(id);
     }
 
     @GetMapping("/search")
+    @ResponseStatus(HttpStatus.OK)
     public List<ItemDtoForCreate> findAvailableByText(@RequestHeader("X-Sharer-User-Id") Long userId,
                                                       @RequestParam String text) {
         return itemService.findAvailableByText(userId, text);
--- src/main/java/ru/practicum/shareit/user/controller/UserController.java
+++ src/main/java/ru/practicum/shareit/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.user.dto.UserDtoForCreate;
 import ru.practicum.shareit.user.dto.UserDtoForUpdate;
@@ -17,27 +18,32 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDtoForCreate create(@Valid @RequestBody UserDtoForCreate userDtoForCreate) {
         return userService.create(userDtoForCreate);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<UserDtoForCreate> getAll() {
         return userService.getAll();
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public UserDtoForCreate getById(@PathVariable Long id) {
         return userService.getById(id);
     }
 
     @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public UserDtoForCreate update(@PathVariable Long id,
                                    @Valid @RequestBody UserDtoForUpdate userDtoForUpdate) {
         return userService.update(id, userDtoForUpdate);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         userService.delete(id);
     }
