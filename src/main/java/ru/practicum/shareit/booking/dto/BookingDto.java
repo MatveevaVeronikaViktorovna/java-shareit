cf85befb18fc806b709e986a96a085da321b41ed
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-06-17 11:17:55 +0300

    fix: исправлено 2 замечание

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -3,10 +3,7 @@ package ru.practicum.shareit.booking.dto;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.shareit.booking.model.Status;
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.dto.Create;
-import ru.practicum.shareit.user.dto.UserDto;
 
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
@@ -16,16 +13,12 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 public class BookingDto {
-    Long id;
     @NotNull(groups = Create.class)
     @FutureOrPresent(groups = Create.class)
     LocalDateTime start;
     @Future(groups = Create.class)
     @NotNull(groups = Create.class)
     LocalDateTime end;
-    UserDto booker;
     @NotNull(groups = Create.class)
     Long itemId;
-    ItemDto item;
-    Status status;
 }
