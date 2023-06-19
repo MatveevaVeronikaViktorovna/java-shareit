e76784356626750bf69a7183c5c8ed10ff3fbd76
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-06-19 11:51:36 +0300

    fix: исправлены 3 замечания

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -16,18 +16,18 @@ import java.util.List;
 public class BookingController {
 
     private final BookingService bookingService;
-    private final String header = "X-Sharer-User-Id";
+    private final String HEADER = "X-Sharer-User-Id";
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookingDtoForResponse create(@RequestHeader(header) Long userId,
+    public BookingDtoForResponse create(@RequestHeader(HEADER) Long userId,
                                         @Valid @RequestBody BookingDto bookingDto) {
         return bookingService.create(userId, bookingDto);
     }
 
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public BookingDtoForResponse approveOrReject(@RequestHeader(header) Long userId,
+    public BookingDtoForResponse approveOrReject(@RequestHeader(HEADER) Long userId,
                                                  @PathVariable Long id,
                                                  @RequestParam Boolean approved) {
         return bookingService.approveOrReject(userId, id, approved);
@@ -35,21 +35,21 @@ public class BookingController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public BookingDtoForResponse getById(@RequestHeader(header) Long userId,
+    public BookingDtoForResponse getById(@RequestHeader(HEADER) Long userId,
                                          @PathVariable Long id) {
         return bookingService.getById(userId, id);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<BookingDtoForResponse> getAllByBooker(@RequestHeader(header) Long userId,
+    public List<BookingDtoForResponse> getAllByBooker(@RequestHeader(HEADER) Long userId,
                                                       @RequestParam(defaultValue = "ALL") State state) {
         return bookingService.getAllByBooker(userId, state);
     }
 
     @GetMapping("/owner")
     @ResponseStatus(HttpStatus.OK)
-    public List<BookingDtoForResponse> getAllByOwner(@RequestHeader(header) Long userId,
+    public List<BookingDtoForResponse> getAllByOwner(@RequestHeader(HEADER) Long userId,
                                                      @RequestParam(defaultValue = "ALL") State state) {
         return bookingService.getAllByOwner(userId, state);
     }
--- src/main/java/ru/practicum/shareit/item/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/controller/ItemController.java
@@ -18,31 +18,31 @@ import java.util.List;
 public class ItemController {
 
     private final ItemService itemService;
-    private final String header = "X-Sharer-User-Id";
+    private final String HEADER = "X-Sharer-User-Id";
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ItemDto create(@RequestHeader(header) Long userId,
+    public ItemDto create(@RequestHeader(HEADER) Long userId,
                           @Validated(Create.class) @RequestBody ItemDto itemDto) {
         return itemService.create(userId, itemDto);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<ItemDto> getAllByOwner(@RequestHeader(header) Long userId) {
+    public List<ItemDto> getAllByOwner(@RequestHeader(HEADER) Long userId) {
         return itemService.getAllByOwner(userId);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ItemDto getById(@RequestHeader(header) Long userId,
+    public ItemDto getById(@RequestHeader(HEADER) Long userId,
                            @PathVariable Long id) {
         return itemService.getById(userId, id);
     }
 
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ItemDto update(@RequestHeader(header) Long userId,
+    public ItemDto update(@RequestHeader(HEADER) Long userId,
                           @PathVariable Long id,
                           @Validated(Update.class) @RequestBody ItemDto itemDto) {
         return itemService.update(userId, id, itemDto);
@@ -56,14 +56,14 @@ public class ItemController {
 
     @GetMapping("/search")
     @ResponseStatus(HttpStatus.OK)
-    public List<ItemDto> searchByText(@RequestHeader(header) Long userId,
+    public List<ItemDto> searchByText(@RequestHeader(HEADER) Long userId,
                                       @RequestParam String text) {
         return itemService.searchByText(userId, text);
     }
 
     @PostMapping("/{itemId}/comment")
     @ResponseStatus(HttpStatus.OK)
-    public CommentDto addComment(@RequestHeader(header) Long userId,
+    public CommentDto addComment(@RequestHeader(HEADER) Long userId,
                                  @PathVariable Long itemId,
                                  @Validated(Create.class) @RequestBody CommentDto commentDto) {
         return itemService.addComment(userId, itemId, commentDto);
