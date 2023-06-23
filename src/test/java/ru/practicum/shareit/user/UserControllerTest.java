4596ba37d224e5ce7b774d9f4fd28cda3d58486b
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-06-23 11:59:18 +0300

    feat: добавила два теста для UserService

---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,53 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private MockMvc mvc;
+
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        mvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .build();
+
+/*        userDto = new UserDto(
+                1L,
+                "John",
+                "john.doe@mail.com"); */
+    }
+
+    // Спринт 15/20: 15 → Тема 1/3: Мок-тестирование → Урок 5/6 Тестирование web-приложений. MockMvc
+
+    @Test
+    void getAll() {
+        List<UserDto> users = userService.getAll();
+
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/user/service/UserServiceImplTest.java
@@ -0,0 +1,56 @@
+package ru.practicum.shareit.user.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserMapper;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    void getAllWhenInvokedThenOk() {
+        List<User> expectedUsers = List.of(new User());
+        Mockito.when(userRepository.findAll()).thenReturn(expectedUsers);
+
+        List<UserDto> users = userService.getAll();
+
+        assertEquals(1, users.size());
+    }
+
+    @Test
+    void getByIdWhenUserFoundThenReturnedUser() {
+        Long id = 0L;
+        User expectedUser = new User();
+        UserDto expectedUserDto = UserMapper.toDto(expectedUser);
+        Mockito.when(userRepository.findById(id)).thenReturn(Optional.of(expectedUser));
+
+        UserDto user = userService.getById(id);
+
+        assertEquals(expectedUserDto, user);
+
+    }
+
+    @Test
+    void getByIdWhenUserNotFoundThenThrownException() {
+
+    }
+
+}
