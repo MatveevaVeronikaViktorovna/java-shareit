41f9bc6f606e6bfe9db3c648839ca4c8a45ec303
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-04 13:12:01 +0300

    feat: написала все тесты для itemService для comment

---
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplTest.java
@@ -0,0 +1,42 @@
+package ru.practicum.shareit.request.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.booking.repository.BookingRepository;
+import ru.practicum.shareit.item.repository.CommentRepository;
+import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.repository.ItemRequestRepository;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ItemRequestServiceImplTest {
+
+    @Mock
+    private ItemRepository itemRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private BookingRepository bookingRepository;
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private ItemRequestRepository itemRequestRepository;
+
+    @InjectMocks
+    private ItemRequestServiceImpl itemRequestService;
+
+    @Captor
+    private ArgumentCaptor<ItemRequest> itemRequestArgumentCaptor;
+
+    @Test
+    void create() {
+    }
+}
