c40186038bcabecf7c9ea733ac2918b774f41d9c
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-06 20:17:21 +0300

    feat: написала интеграционные тесты для всех сервисов

--- src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceImplIntegrationTest.java
@@ -4,12 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.dto.UserMapper;
+import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Transactional
 @SpringBootTest
@@ -21,5 +27,26 @@ class ItemRequestServiceImplIntegrationTest {
 
     @Test
     void create() {
+        User user1 = new User();
+        user1.setName("name1");
+        user1.setEmail("name1@yandex.ru");
+        em.persist(user1);
+        em.flush();
+
+        ItemRequestDto itemRequestDto = new ItemRequestDto();
+        itemRequestDto.setDescription("description");
+        itemRequestDto.setRequestor(UserMapper.toDto(user1));
+
+        service.create(user1.getId(), itemRequestDto);
+
+        TypedQuery<ItemRequest> query = em.createQuery("Select ir from ItemRequest ir where ir.description = :description", ItemRequest.class);
+        ItemRequest itemRequest = query
+                .setParameter("description", itemRequestDto.getDescription())
+                .getSingleResult();
+
+        assertThat(itemRequest.getId(), notNullValue());
+        assertThat(itemRequest.getDescription(), equalTo(itemRequestDto.getDescription()));
+        assertThat(itemRequest.getRequestor().getId(), equalTo(itemRequestDto.getRequestor().getId()));
+        assertThat(itemRequest.getCreated(), notNullValue());
     }
 }
