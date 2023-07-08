768e041d6f5fc574b65fe34f96f6813d74fb1f49
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-08 13:45:46 +0300

    fix: обработано 5 комментариев ревьюера

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.item.model.Item;
 
@@ -12,9 +13,9 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findAllByOwnerIdOrderByIdAsc(Long userId, Pageable pageable);
 
-    @Query("select i from Item i where ((lower(i.name) like lower(concat('%', ?1, '%')) or lower(i.description) like" +
-            " lower(concat('%', ?1, '%'))) and i.available = true)")
-    List<Item> findAllAvailableByText(String text, Pageable pageable);
+    @Query("select i from Item i where ((lower(i.name) like lower(concat('%', :text, '%')) or lower(i.description) like" +
+            " lower(concat('%', :text, '%'))) and i.available = true)")
+    List<Item> findAllAvailableByText(@Param("text") String text, Pageable pageable);
 
     List<Item> findAllByRequestIdOrderByIdAsc(Long requestId);
 }
