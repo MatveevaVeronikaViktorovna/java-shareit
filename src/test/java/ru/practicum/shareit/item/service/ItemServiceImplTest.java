b49c1ecd0ef6be192833fd0cf7bb4183bf6828e1
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-08 18:35:27 +0300

    fix: выделяю beforeEach в ItemService

--- src/test/java/ru/practicum/shareit/item/service/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/service/ItemServiceImplTest.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
@@ -20,6 +21,7 @@ import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.CommentRepository;
 import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.pagination.CustomPageRequest;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.request.repository.ItemRequestRepository;
 import ru.practicum.shareit.user.model.User;
@@ -29,6 +31,7 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.IntPredicate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -53,18 +56,62 @@ class ItemServiceImplTest {
 
     @Captor
     private ArgumentCaptor<Item> itemArgumentCaptor;
+    private Long ownerId;
+    private User owner;
+    Long requestId;
+    ItemRequest request;
+    private Long id;
+    private Item expectedItem;
+    private ItemDto expectedItemDto;
+    Booking lastBooking;
+    Booking nextBooking;
+    private Integer from;
+    private Integer size;
+    Pageable page;
+
+
+    @BeforeEach
+    public void addItems() {
+        ownerId = 0L;
+        owner = new User();
+        owner.setId(ownerId);
+
+        requestId = 0L;
+        request = new ItemRequest();
+        request.setId(requestId);
+
+        id = 0L;
+        expectedItem = new Item();
+        expectedItem.setId(id);
+        expectedItem.setName("name");
+        expectedItem.setDescription("description");
+        expectedItem.setAvailable(true);
+        expectedItem.setOwner(owner);
+
+
+        lastBooking = new Booking();
+        User booker = new User();
+        booker.setId(1L);
+        lastBooking.setBooker(booker);
+        nextBooking = new Booking();
+        nextBooking.setBooker(booker);
+
+        expectedItemDto = ItemMapper.toDto(expectedItem);
+        expectedItemDto.setComments(Collections.emptyList());
+        expectedItemDto.setLastBooking(BookingMapper.toDtoForItem(lastBooking));
+        expectedItemDto.setNextBooking(BookingMapper.toDtoForItem(nextBooking));
+
+        from = 0;
+        size = 10;
+        page = CustomPageRequest.of(from, size);
+    }
 
     @Test
     void createWhenOwnerFoundThenSavedItem() {
-        Long id = 0L;
-        User owner = new User();
-        Item expectedItem = new Item();
-        expectedItem.setOwner(owner);
-        ItemDto expectedItemDto = ItemMapper.toDto(expectedItem);
-        Mockito.when(userRepository.findById(id)).thenReturn(Optional.of(owner));
+        Mockito.when(userRepository.findById(ownerId)).thenReturn(Optional.of(owner));
         Mockito.when(itemRepository.save(Mockito.any(Item.class))).thenReturn(expectedItem);
 
-        ItemDto item = itemService.create(id, expectedItemDto);
+        ItemDto item = itemService.create(ownerId, expectedItemDto);
 
         assertEquals(expectedItemDto, item);
         verify(itemRepository).save(expectedItem);
@@ -72,32 +119,22 @@ class ItemServiceImplTest {
 
     @Test
     void createWhenOwnerNotFoundThenNotSavedItem() {
-        Long id = 0L;
-        Item expectedItem = new Item();
-        ItemDto expectedItemDto = ItemMapper.toDto(expectedItem);
-        Mockito.when(userRepository.findById(id)).thenReturn(Optional.empty());
+        Mockito.when(userRepository.findById(ownerId)).thenReturn(Optional.empty());
 
-        assertThrows(EntityNotFoundException.class, () -> itemService.create(id, expectedItemDto));
+        assertThrows(EntityNotFoundException.class, () -> itemService.create(ownerId, expectedItemDto));
 
         verify(itemRepository, Mockito.never()).save(expectedItem);
     }
 
     @Test
     void createWhenOwnerFoundAndRequestIdNotNullThenSavedItem() {
-        Long id = 0L;
-        User owner = new User();
-        Long requestId = 0L;
-        ItemRequest request = new ItemRequest();
-        request.setId(requestId);
-        Item expectedItem = new Item();
-        expectedItem.setOwner(owner);
         expectedItem.setRequest(request);
-        ItemDto expectedItemDto = ItemMapper.toDto(expectedItem);
-        Mockito.when(userRepository.findById(id)).thenReturn(Optional.of(owner));
+        expectedItemDto = ItemMapper.toDto(expectedItem);
+        Mockito.when(userRepository.findById(ownerId)).thenReturn(Optional.of(owner));
         Mockito.when(itemRequestRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(request));
         Mockito.when(itemRepository.save(Mockito.any(Item.class))).thenReturn(expectedItem);
 
-        ItemDto item = itemService.create(id, expectedItemDto);
+        ItemDto item = itemService.create(ownerId, expectedItemDto);
 
         assertEquals(expectedItemDto, item);
         verify(itemRepository).save(expectedItem);
@@ -105,31 +142,19 @@ class ItemServiceImplTest {
 
     @Test
     void getAllByOwnerWhenInvokedThenReturnedListOfItems() {
-        Long userId = 0L;
-        Integer from = 0;
-        Integer size = 10;
-        Pageable page = PageRequest.of(from / size, size);
         List<Item> expectedItems = List.of(new Item());
         List<ItemDto> expectedItemsDto = ItemMapper.toDto(expectedItems);
         expectedItemsDto.forEach(itemDto -> itemDto.setComments(Collections.emptyList()));
-        Mockito.when(itemRepository.findAllByOwnerIdOrderByIdAsc(userId, page)).thenReturn(expectedItems);
+        Mockito.when(itemRepository.findAllByOwnerIdOrderByIdAsc(ownerId, page)).thenReturn(expectedItems);
 
-        List<ItemDto> items = itemService.getAllByOwner(userId, from, size);
+        List<ItemDto> items = itemService.getAllByOwner(ownerId, from, size);
 
         assertEquals(expectedItemsDto, items);
     }
 
     @Test
     void getByIdWhenItemFoundAndRequestNotFromOwnerThenReturnedItem() {
-        Long id = 0L;
-        Long userId = 0L;
-        Long ownerId = 1L;
-        User owner = new User();
-        owner.setId(ownerId);
-        Item expectedItem = new Item();
-        expectedItem.setOwner(owner);
-        ItemDto expectedItemDto = ItemMapper.toDto(expectedItem);
-        expectedItemDto.setComments(Collections.emptyList());
+        Long userId = 99L;
         Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(expectedItem));
 
         ItemDto item = itemService.getById(userId, id);
@@ -139,26 +164,6 @@ class ItemServiceImplTest {
 
     @Test
     void getByIdWhenItemFoundAndRequestFromOwnerThenReturnedItemWithLastBooking() {
-        Long ownerId = 0L;
-        User owner = new User();
-        owner.setId(ownerId);
-
-        Booking lastBooking = new Booking();
-        User booker = new User();
-        booker.setId(1L);
-        lastBooking.setBooker(booker);
-        Booking nextBooking = new Booking();
-        nextBooking.setBooker(booker);
-
-        Long id = 0L;
-        Item expectedItem = new Item();
-        expectedItem.setId(id);
-        expectedItem.setOwner(owner);
-        ItemDto expectedItemDto = ItemMapper.toDto(expectedItem);
-        expectedItemDto.setComments(Collections.emptyList());
-        expectedItemDto.setLastBooking(BookingMapper.toDtoForItem(lastBooking));
-        expectedItemDto.setNextBooking(BookingMapper.toDtoForItem(nextBooking));
-
         Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(expectedItem));
         Mockito.when(bookingRepository.findFirstByItemIdAndStartBeforeAndStatusOrderByStartDesc(Mockito.anyLong(),
                 Mockito.any(LocalDateTime.class), Mockito.any(Status.class))).thenReturn(Optional.of(lastBooking));
@@ -172,35 +177,21 @@ class ItemServiceImplTest {
 
     @Test
     void getByIdWhenItemNotFoundThenNotReturnedItem() {
-        Long id = 0L;
-        Long userId = 0L;
         Mockito.when(itemRepository.findById(id)).thenReturn(Optional.empty());
 
-        assertThrows(EntityNotFoundException.class, () -> itemService.getById(userId, id));
+        assertThrows(EntityNotFoundException.class, () -> itemService.getById(ownerId, id));
     }
 
     @Test
     void updateWhenItemFoundAndOwnerCorrectThenItemUpdated() {
-        Long userId = 0L;
-        User owner = new User();
-        owner.setId(userId);
-
-        Long id = 0L;
-        Item oldItem = new Item();
-        oldItem.setId(id);
-        oldItem.setName("name");
-        oldItem.setDescription("description");
-        oldItem.setAvailable(true);
-        oldItem.setOwner(owner);
-
         Item newItem = new Item();
         newItem.setName("updatedName");
         newItem.setDescription("updated description");
         newItem.setAvailable(false);
         ItemDto newItemDto = ItemMapper.toDto(newItem);
-        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(oldItem));
+        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(expectedItem));
 
-        itemService.update(userId, id, newItemDto);
+        itemService.update(ownerId, id, newItemDto);
 
         verify(itemRepository).save(itemArgumentCaptor.capture());
         Item savedItem = itemArgumentCaptor.getValue();
@@ -211,18 +202,10 @@ class ItemServiceImplTest {
 
     @Test
     void updateWhenItemFoundButOwnerWrongThenItemNotUpdated() {
-        Long userId = 0L;
-        Long ownerId = 1L;
-        User owner = new User();
-        owner.setId(ownerId);
-
-        Long id = 0L;
-        Item oldItem = new Item();
-        oldItem.setOwner(owner);
-
+        Long userId = 99L;
         Item newItem = new Item();
         ItemDto newItemDto = ItemMapper.toDto(newItem);
-        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(oldItem));
+        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(expectedItem));
 
         assertThrows(EntityNotFoundException.class, () -> itemService.update(userId, id, newItemDto));
         verify(itemRepository, Mockito.never()).save(Mockito.any(Item.class));
@@ -230,24 +213,12 @@ class ItemServiceImplTest {
 
     @Test
     void updateNameWhenItemFoundAndOwnerCorrectThenUpdatedOnlyName() {
-        Long userId = 0L;
-        User owner = new User();
-        owner.setId(userId);
-
-        Long id = 0L;
-        Item oldItem = new Item();
-        oldItem.setId(id);
-        oldItem.setName("name");
-        oldItem.setDescription("description");
-        oldItem.setAvailable(true);
-        oldItem.setOwner(owner);
-
         Item newItem = new Item();
         newItem.setName("updatedName");
         ItemDto newItemDto = ItemMapper.toDto(newItem);
-        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(oldItem));
+        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(expectedItem));
 
-        itemService.update(userId, id, newItemDto);
+        itemService.update(ownerId, id, newItemDto);
 
         verify(itemRepository).save(itemArgumentCaptor.capture());
         Item savedItem = itemArgumentCaptor.getValue();
@@ -258,24 +229,12 @@ class ItemServiceImplTest {
 
     @Test
     void updateDescriptionWhenItemFoundAndOwnerCorrectThenUpdatedOnlyDescription() {
-        Long userId = 0L;
-        User owner = new User();
-        owner.setId(userId);
-
-        Long id = 0L;
-        Item oldItem = new Item();
-        oldItem.setId(id);
-        oldItem.setName("name");
-        oldItem.setDescription("description");
-        oldItem.setAvailable(true);
-        oldItem.setOwner(owner);
-
         Item newItem = new Item();
         newItem.setDescription("updated description");
         ItemDto newItemDto = ItemMapper.toDto(newItem);
-        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(oldItem));
+        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(expectedItem));
 
-        itemService.update(userId, id, newItemDto);
+        itemService.update(ownerId, id, newItemDto);
 
         verify(itemRepository).save(itemArgumentCaptor.capture());
         Item savedItem = itemArgumentCaptor.getValue();
@@ -286,24 +245,12 @@ class ItemServiceImplTest {
 
     @Test
     void updateAvailableWhenItemFoundAndOwnerCorrectThenUpdatedOnlyAvailable() {
-        Long userId = 0L;
-        User owner = new User();
-        owner.setId(userId);
-
-        Long id = 0L;
-        Item oldItem = new Item();
-        oldItem.setId(id);
-        oldItem.setName("name");
-        oldItem.setDescription("description");
-        oldItem.setAvailable(true);
-        oldItem.setOwner(owner);
-
         Item newItem = new Item();
         newItem.setAvailable(false);
         ItemDto newItemDto = ItemMapper.toDto(newItem);
-        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(oldItem));
+        Mockito.when(itemRepository.findById(id)).thenReturn(Optional.of(expectedItem));
 
-        itemService.update(userId, id, newItemDto);
+        itemService.update(ownerId, id, newItemDto);
 
         verify(itemRepository).save(itemArgumentCaptor.capture());
         Item savedItem = itemArgumentCaptor.getValue();
@@ -313,58 +260,39 @@ class ItemServiceImplTest {
     }
 
     @Test
-    void updateWhenOwnerNotFoundThenNotUpdatedItem() {
-        Long userId = 0L;
-        User owner = new User();
-        owner.setId(userId);
-
-        Long id = 0L;
-        Item oldItem = new Item();
-        oldItem.setOwner(owner);
-
+    void updateWhenItemNotFoundThenNotUpdatedItem() {
         Item newItem = new Item();
         ItemDto newItemDto = ItemMapper.toDto(newItem);
         Mockito.when(itemRepository.findById(id)).thenReturn(Optional.empty());
 
-        assertThrows(EntityNotFoundException.class, () -> itemService.update(userId, id, newItemDto));
+        assertThrows(EntityNotFoundException.class, () -> itemService.update(ownerId, id, newItemDto));
         verify(itemRepository, Mockito.never()).save(Mockito.any(Item.class));
     }
 
     @Test
     void deleteWhenInvokedThenDeleteItem() {
-        Long id = 0L;
         itemService.delete(id);
+
         verify(itemRepository, Mockito.times(1)).deleteById(id);
     }
 
     @Test
     void searchByTextWhenInvokedThenReturnedListOfItems() {
-        Long userId = 0L;
-        Long id = 0L;
-        Item item = new Item();
-        item.setName("name");
-        item.setDescription("description");
         String text = "eSC";
-        Integer from = 0;
-        Integer size = 10;
-        Pageable page = PageRequest.of(from / size, size);
-        List<Item> expectedItems = List.of(item);
+        List<Item> expectedItems = List.of(expectedItem);
         List<ItemDto> expectedItemsDto = ItemMapper.toDto(expectedItems);
         Mockito.when(itemRepository.findAllAvailableByText(text, page)).thenReturn(expectedItems);
 
-        List<ItemDto> items = itemService.searchByText(userId, text, from, size);
+        List<ItemDto> items = itemService.searchByText(ownerId, text, from, size);
 
         assertEquals(expectedItemsDto, items);
     }
 
     @Test
-    void searchByyTextWhenTextEmptyThenReturnedListOfItems() {
-        Long userId = 0L;
+    void searchByyTextWhenTextEmptyThenReturnedEmptyList() {
         String text = "";
-        Integer from = 0;
-        Integer size = 10;
 
-        List<ItemDto> items = itemService.searchByText(userId, text, from, size);
+        List<ItemDto> items = itemService.searchByText(ownerId, text, from, size);
 
         assertEquals(Collections.emptyList(), items);
     }
