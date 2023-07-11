05afb18311003d4e4192f8566eec8be5dc17190f
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-11 12:06:31 +0300

    fix: реализовала MapStruct для всех сущностей, исправлены косяки

--- src/main/java/ru/practicum/shareit/item/dto/CommentDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/CommentDtoMapper.java
@@ -4,7 +4,14 @@ import org.mapstruct.Mapper;
 import ru.practicum.shareit.item.model.Comment;
 
 @Mapper
-public interface CommentDtoMapper {
-    Comment dtoToComment(CommentDto dto);
-    CommentDto commentToDto(Comment comment);
+public abstract class CommentDtoMapper {
+    public abstract Comment dtoToComment(CommentDto dto);
+    public CommentDto commentToDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setText(comment.getText());
+        commentDto.setAuthorName(comment.getAuthor().getName());
+        commentDto.setCreated(comment.getCreated());
+        return commentDto;
+    }
 }
--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoMapper.java
@@ -6,9 +6,27 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 @Mapper
-public interface ItemDtoMapper {
-    Item dtoToItem(ItemDto dto);
-    ItemDto itemToDto(Item item);
-    List<ItemDto> itemToDto(List<Item> items);
-    ItemDtoForItemRequest itemToDtoForItemRequest(Item item);
+public abstract class ItemDtoMapper {
+    public abstract Item dtoToItem(ItemDto dto);
+    public ItemDto itemToDto(Item item){
+        ItemDto itemDto = new ItemDto();
+        itemDto.setId(item.getId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.getAvailable());
+        if (item.getRequest() != null) {
+            itemDto.setRequestId(item.getRequest().getId());
+        }
+        return itemDto;
+    }
+    public abstract List<ItemDto> itemToDto(List<Item> items);
+    public ItemDtoForItemRequest itemToDtoForItemRequest(Item item){
+        ItemDtoForItemRequest itemDto = new ItemDtoForItemRequest();
+        itemDto.setId(item.getId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.getAvailable());
+        itemDto.setRequestId(item.getRequest().getId());
+        return itemDto;
+    }
 }
