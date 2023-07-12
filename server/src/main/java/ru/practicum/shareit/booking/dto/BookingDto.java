bc0df3204b3309e39a45fd17177595c6a08d9195
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-12 17:01:50 +0300

    правлю server, ошибки в тестах 6

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -3,13 +3,11 @@ package ru.practicum.shareit.booking.dto;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.shareit.valid.StartBeforeEndDateValid;
 
 import java.time.LocalDateTime;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
-@StartBeforeEndDateValid
 public class BookingDto {
     LocalDateTime start;
     LocalDateTime end;
