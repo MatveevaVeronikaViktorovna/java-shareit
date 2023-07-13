e4e210cb2de3a3cfde54ee855484e2718cfcf47b
Veronika Matveeva <ve-ro-ni-ka-IV@yandex.ru>
2023-07-13 13:39:12 +0300

    fix: отладка работы приложения, частично перенесла errorHandler в gateway

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.exception;
 
-import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -26,12 +25,6 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleItemNotAvailableException(final ItemNotAvailableException e) {
         return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleConversionFailedException(final ConversionFailedException e) {
-        return new ErrorResponse("Unknown state: UNSUPPORTED_STATUS");
     }
 
     @ExceptionHandler
